e7a0929e5efeab5b1078b9aa148b58aba643b771
이 동건 <87507644+donggeonL@users.noreply.github.com>
2021-08-27 12:15:54 +0900

    Delete demo directory

--- demo/src/main/java/com/example/demo/repository/userRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.User;
-
-public interface userRepository extends JpaRepository<User, Long>{
-
-}
